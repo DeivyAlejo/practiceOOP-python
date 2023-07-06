e8e6004488e769fa0a38baf3c14a3d1376fbbef4
Deivy <deivy.munoz-acosta@mohawkcollege.ca>
2023-07-06 14:48:44 -0400

    Class methods created

--- Exercise3/OldIrelandWorld.py
+++ Exercise3/OldIrelandWorld.py
@@ -1,13 +1,48 @@
-class Inhabitant():
+import random
+from abc import ABC
+
+class Inhabitant(ABC):
     
     def __init__(self, name):
-        self._name = name
+        self.name = name
+        self.health = 10
+        self.strength = random.randint(1,10)
+        self.agility = random.randint(1,10)
+        self.armour = random.randint(1,10)
+        self.alive = True
     
-    @property
-    def name(self):
-        return self._name
+    def increase_health(self):
+        if self.health < 10 and self.alive:
+            self.health += 1
     
+    def increase_strength(self):
+        if self.strength < 10 and self.alive:
+            self.strength += 1
+
+    def increase_agility(self):
+        if self.agility < 10 and self.alive:
+            self.agility += 1
     
+    def increase_armour(self):
+        if self.armour < 10 and self.alive:
+            self.armour += 1
+
+    def decrease_health(self):
+            self.health -= 1
+            if self.health == 0:
+                self.alive = False
+    
+    def decrease_strength(self):
+        if self.strength > 0 and self.alive:
+            self.strength += 1
+
+    def decrease_agility(self):
+        if self.agility < 10 and self.alive:
+            self.agility += 1
+    
+    def decrease_armour(self):
+        if self.armour < 10 and self.alive:
+            self.armour += 1
 
 
 class Werewolf(Inhabitant):
